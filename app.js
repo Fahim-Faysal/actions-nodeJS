5a1e5edf85f659da611a9157fe7c194c0be1b355
siyam <faysalsiyam@gmail.com>
2025-11-11 14:53:11 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('🚀 Hello from Node CI/CD pipeline!'));
+
+app.listen(port, () => console.log(`App running on port ${port}`));
+
+module.exports = app; // for testing
